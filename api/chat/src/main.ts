eb611ae87c8376034d29100bd64bff82c8642e98
kylon-tyner-fis <kylon.tyner@flatironschool.com>
2026-02-13 11:28:57 -0500

    exporting server instead of listening for vercel architecture

--- api/chat/src/main.ts
+++ api/chat/src/main.ts
@@ -12,12 +12,14 @@ const server = Fastify({
 // Register your application as a normal plugin.
 server.register(app);
 
-// Start listening.
-server.listen({ port, host }, (err) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  } else {
-    console.log(`[ ready ] http://${host}:${port}`);
-  }
-});
+// // Start listening.
+// server.listen({ port, host }, (err) => {
+//   if (err) {
+//     server.log.error(err);
+//     process.exit(1);
+//   } else {
+//     console.log(`[ ready ] http://${host}:${port}`);
+//   }
+// });
+
+export default server;
